24b511038ddeef1c67f143ff2e307ee8a8b59438
Alandriya <ashcherbinina@mail.ru>
2024-11-05 11:40:00 +0300

    ml_runner with torch load

--- ml_runner.py
+++ ml_runner.py
@@ -10,10 +10,15 @@ from sklearn.ensemble import GradientBoostingRegressor
 from skimage.metrics import structural_similarity as ssim
 # import tensorflow as tf
 import sys
+import torch
+# SHORT_POSTFIX = '_short'
+SHORT_POSTFIX = ''
+files_path_prefix = 'E:/Nastya/Data/OceanFull/'
+from config import cfg
+import copy
+
 
 if __name__ == '__main__':
-    # files_path_prefix = 'E:/Nastya/Data/OceanFull/'
-    files_path_prefix = 'D:/Programming/PythonProjects/Alana/Data/'
     np.set_printoptions(threshold=sys.maxsize)
     # --------------------------------------------------------------------------------
     # Mask
@@ -23,6 +28,8 @@ if __name__ == '__main__':
     mask = unpack('?' * 29141, binary_values)
     mask = np.array(mask, dtype=int)
     mask = mask.reshape((161, 181))[::2, ::2]
+    print(np.sum(mask))
+    # print(cfg.batch)
     # ---------------------------------------------------------------------------------------
     # Days deltas
     days_delta1 = (datetime.datetime(1989, 1, 1, 0, 0) - datetime.datetime(1979, 1, 1, 0, 0)).days
@@ -43,90 +50,91 @@ if __name__ == '__main__':
     # configs
     width = 91
     height = 81
-    batch_size = 16
+    batch_size = cfg.batch
     days_known = 7
     days_prediction = 5
-    features_amount = 3
+    features_amount = 6
     # ---------------------------------------------------------------------------------------
-    x_mins = np.load(files_path_prefix + f'Forecast/Normalize/x_mins_{start_year}-{end_year}_6.npy')
-    x_maxs = np.load(files_path_prefix + f'Forecast/Normalize/x_maxs_{start_year}-{end_year}_6.npy')
-    y_mins = np.load(files_path_prefix + f'Forecast/Normalize/y_mins_{start_year}-{end_year}.npy')
-    y_maxs = np.load(files_path_prefix + f'Forecast/Normalize/y_maxs_{start_year}-{end_year}.npy')
-    print(x_mins)
-    print(x_maxs)
-    print(y_mins)
-    print(y_maxs)
-    # raise ValueError
+    x_train = torch.load(files_path_prefix + f'Forecast/Train/{start_year}-{end_year}_x_train_{features_amount}{SHORT_POSTFIX}.pt')
+    y_train = torch.load(files_path_prefix + f'Forecast/Train/{start_year}-{end_year}_y_train_{features_amount}{SHORT_POSTFIX}.pt')
 
-    x_train = np.load(files_path_prefix + f'Forecast/Train/{start_year}-{end_year}_x_train_Unet_{features_amount}.npy')
-    y_train = np.load(files_path_prefix + f'Forecast/Train/{start_year}-{end_year}_y_train_Unet_{features_amount}.npy')
+    x_test = torch.load(files_path_prefix + f'Forecast/Test/{start_year}-{end_year}_x_test_{features_amount}{SHORT_POSTFIX}.pt')
+    y_test = torch.load(files_path_prefix + f'Forecast/Test/{start_year}-{end_year}_y_test_{features_amount}{SHORT_POSTFIX}.pt')
 
-    x_test = np.load(files_path_prefix + f'Forecast/Test/{start_year}-{end_year}_x_test_Unet_{features_amount}.npy')
-    y_test = np.load(files_path_prefix + f'Forecast/Test/{start_year}-{end_year}_y_test_Unet_{features_amount}.npy')
+    min_vals = torch.amin(y_train, dim=(0, 1, 2, 3)).numpy()
+    max_vals = torch.amax(y_train, dim=(0, 1, 2, 3)).numpy()
+    # print(min_vals)
+    # print(max_vals)
 
-    x_train[:, np.logical_not(mask)] = 0
-    x_test[:, np.logical_not(mask)] = 0
-    y_train[:, np.logical_not(mask)] = 0
-    y_test[:, np.logical_not(mask)] = 0
+    x_train = torch.permute(x_train, dims=(0, 3, 4, 1, 2)).numpy()
+    y_train = torch.permute(y_train, dims=(0, 3, 4, 1, 2)).numpy()
+    x_test = torch.permute(x_test, dims=(0, 3, 4, 1, 2)).numpy()
+    y_test = torch.permute(y_test, dims=(0, 3, 4, 1, 2)).numpy()
+    y_test_copy = copy.deepcopy(y_test)
 
-    flux_error, sst_error, press_error = 0, 0, 0
-    # get dumb prediction and plot it
-    for t in range(10):
-        y_pred = np.mean(y_train[-days_known:, :, :, :, :], axis=0)
-        start_day = datetime.datetime(1979, 1, 1) + datetime.timedelta(days=offset + x_train.shape[0] + t)
-        # plot_predictions(files_path_prefix, y_test[t], y_pred, 'Dumb', features_amount, start_day, mask)
-        y_pred[np.logical_not(mask)] = 0
+    for k in range(3):
+        x_train[:, :, k] = (x_train[:, :, k] - min_vals[k]) / (max_vals[k] - min_vals[k])
+        y_train[:, :, k] = (y_train[:, :, k] - min_vals[k]) / (max_vals[k] - min_vals[k])
+        x_test[:, :, k] = (x_test[:, :, k] - min_vals[k]) / (max_vals[k] - min_vals[k])
+        y_test[:, :, k] = (y_test[:, :, k] - min_vals[k]) / (max_vals[k] - min_vals[k])
 
-        for t1 in range(days_prediction):
-            # flux_error += np.sqrt(mean_squared_error(y_pred[t1, :, :, 0], y_test[t, t1, :, :, 0]))
-            # sst_error += np.sqrt(mean_squared_error(y_pred[t1, :, :, 1], y_test[t, t1, :, :, 1]))
-            # press_error += np.sqrt(mean_squared_error(y_pred[t1, :, :, 2], y_test[t, t1, :, :, 2]))
-            flux_error += ssim(np.array(y_pred[:, :, t1, 0]), np.array(y_test[t, :, :, t1, 0]), data_range=1)
-            sst_error +=ssim(y_pred[:, :, t1, 1], y_test[t, :, :, t1, 1], data_range=1)
-            press_error += ssim(y_pred[:, :, t1, 2], y_test[t, :, :, t1, 2], data_range=1)
+    print(x_train.shape)
+    # mse = 0
+    # mae = 0
+    # ssim_arr = np.zeros((batch_size, days_prediction, 3))
+    # # get dumb prediction and plot it
+    # for t in range(batch_size):
+    #     y_pred = np.zeros((days_prediction, 3, height, width))
+    #     y_pred[:, :, np.logical_not(mask)] = 0
+    #     for t1 in range(days_prediction):
+    #         y_pred[t1] = np.mean(y_train[t], axis=0)[:3]
+    #         for k in range(3):
+    #             ssim_arr[t, t1, k] = ssim(y_pred[t1, k], y_test[t, t1, k])
+    #
+    #     start_day = datetime.datetime(2019, 1, 1) + datetime.timedelta(days=x_train.shape[0] + t)
+    #     mse += np.sum((y_pred - y_test[t]) ** 2)
+    #     mae += np.sum(np.abs(y_pred-y_test[t]))
+    #     # print(start_day)
+    #     for k in range(3):
+    #         y_pred[:, k] = y_pred[:, k] * (max_vals[k] - min_vals[k]) + min_vals[k]
+    #     plot_predictions(files_path_prefix, y_test_copy[t], y_pred, 'Dumb', features_amount, start_day, mask, cfg)
+    #
+    # print(f'Test SSIM DUMB: {np.mean(ssim_arr, axis=(0, 1))}')
+    # mse = mse/np.sum(mask)/batch_size/3/cfg.out_len
+    # mae = mae/np.sum(mask)/batch_size/3/cfg.out_len
+    # print(f'Test MSE DUMB: {mse:.5f}')
+    # print(f'Test MAE DUMB: {mae:.5f}')
 
-    flux_error /= days_prediction
-    sst_error /= days_prediction
-    press_error /= days_prediction
-    flux_error /= 10
-    sst_error /= 10
-    press_error /= 10
-
-    print(f'Mean Flux SSIM = {flux_error * 100: .2f}')
-    print(f'Mean SST SSIM = {sst_error * 100: .2f}')
-    print(f'Mean Press SSIM = {press_error * 100: .2f}')
-
-    flux_error, sst_error, press_error = 0, 0, 0
+    ssim_arr = np.zeros((batch_size, days_prediction, 3))
+    mse = 0
+    mae = 0
     # get regression prediction and plot it
-    for t in range(10):
-        y_pred = np.zeros((height, width, days_prediction, 3), dtype=float)
+    for t in range(batch_size):
+        y_pred = np.zeros((days_prediction, 3, height, width), dtype=float)
         for k in range(3):
             for i in range(height):
                 for j in range(width):
                     if mask[i, j]:
                         regr = linear_model.LinearRegression()
-                        regr.fit(x_train[:, i, j, -days_prediction:].reshape((-1, features_amount)), y_train[:, i, j, :days_prediction, k].flatten())
-                        y_pred[i, j, :, k] = regr.predict(x_test[0:1, i, j, -days_prediction:].reshape((-1, features_amount)))
-        start_day = datetime.datetime(1979, 1, 1) + datetime.timedelta(days=offset + x_train.shape[0] + t)
-        plot_predictions(files_path_prefix, y_test[t], y_pred, 'Linear regression', features_amount, start_day, mask)
-
-        y_pred = np.array(y_pred, dtype=float)
+                        x_tmp = x_train[:, -days_prediction:, :, i, j].reshape((-1, features_amount))
+                        y_tmp = y_train[:, -days_prediction:, k, i, j].flatten()
+                        # print(x_tmp.shape)
+                        # print(y_tmp.shape)
+                        regr.fit(x_tmp, y_tmp)
+                        y_pred[:, k, i, j] = regr.predict(x_test[0:1, -days_prediction:, :, i, j].reshape((-1, features_amount)))
+        start_day = datetime.datetime(2019, 1, 1) + datetime.timedelta(days=x_train.shape[0] + t)
+        mse += np.sum((y_pred - y_test[t][:, :3]) ** 2)
+        mae += np.sum(np.abs(y_pred-y_test[t][:, :3]))
+        y_pred[:, :, np.logical_not(mask)] = 0
         for t1 in range(days_prediction):
-            # flux_error += np.sqrt(mean_squared_error(y_pred[:, :, t1, 0], y_test[t, :, :, t1, 0]))
-            # sst_error += np.sqrt(mean_squared_error(y_pred[:, :, t1, 1], y_test[t, :, :, t1, 1]))
-            # press_error += np.sqrt(mean_squared_error(y_pred[:, :, t1, 2], y_test[t, :, :, t1, 2]))
-            flux_error += ssim(y_pred[:, :, t1, 0], y_test[t, :, :, t1, 0], data_range=1)
-            sst_error += ssim(y_pred[:, :, t1, 1], y_test[t, :, :, t1, 1], data_range=1)
-            press_error += ssim(y_pred[:, :, t1, 2], y_test[t, :, :, t1, 2], data_range=1)
+            for k in range(3):
+                ssim_arr[t, t1, k] = ssim(y_pred[t1, k], y_test[t, t1, k])
+        for k in range(3):
+            y_pred[:, k] = y_pred[:, k] * (max_vals[k] - min_vals[k]) + min_vals[k]
+        plot_predictions(files_path_prefix, y_test_copy[t][:, :3], y_pred, 'Linear regression', features_amount, start_day, mask, cfg)
 
-    flux_error /= days_prediction
-    sst_error /= days_prediction
-    press_error /= days_prediction
-    flux_error /= 10
-    sst_error /= 10
-    press_error /= 10
-
-    print(f'Regression Flux SSIM = {flux_error * 100: .2f}')
-    print(f'Regression SST SSIM = {sst_error * 100: .2f}')
-    print(f'Regression Press SSIM = {press_error * 100: .2f}')
-
+    mse = mse/np.sum(mask)/batch_size/3/cfg.out_len
+    mae = mae/np.sum(mask)/batch_size/3/cfg.out_len
+    print(f'Test MSE regression: {mse:.5f}')
+    print(f'Test MAE regression: {mae:.5f}')
+    print(f'Test SSIM regression: {np.mean(ssim_arr, axis=(0, 1))}')
